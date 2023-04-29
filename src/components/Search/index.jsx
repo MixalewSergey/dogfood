5c70c1fc92420c5f0e3c2cc6d231afe29781dc2a
MixalewSergey <mixalewsergey@yandex.ru>
2023-04-29 05:40:15 +0300

    откат

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,41 +2,71 @@ import { useState } from "react";
 import "./style.css";
 
 const Search = () => {
-    //let text = "Corn";
-    const [text, setText] = useState("Corn")
-    /** userState = то , что создает пару из переменной и
-     *  функции ,которая ее изменяет ,
+    let text = "Corn";
+    /**
+     * useState= то что создает пару из
+     * переменной и функции которая её изменяет
      * 
-     * единсвенный аргумент userState- значение по умолчанию
+     * Единственный аргумент userState-
+     * значение по умолчанию которая сохроняеться
+     * в скобках и передаеться в значение {count}-
+     * -в этом примере
      */
-    const [count, updataCount] = useState(0);
+    const [count, updateCount] = useState(0);
+
     let n = 1;
     const click = () => {
-        const click=()=>{}
-        const saerchByText= (e) =>{
-            //e.target (e.currentTarget)- обращение к тегу ,
-            //на  котором висит событие
-            console.log(e.target.value);
-            setText(e.target.value);
-        }
-        // console.log(n++);
-        updataCount(count + 1);
-        //console.log("count", count)// новое состояние
+        console.log(n++);
         /**
-         * Вызов функцию updateCount, мы гоорим приложени
-         * что при следующем монтаже его параметр count  
-         * будет равен новому значению
+         * Вызывая фунткцию updateCount, мы говорим приложению
+         * Что при следующем монтаже его параметр  count 
+         * равен новому значению 
          */
+        updateCount(count+1);//новое состояние
     }
     return (
         <div className="search-block">
-            <input type="search" value={text}
-            onChange={saerchByText} />
-            <button onClick={click}>Кнопочка</button>
+            <input type="search" />
+            <button onClick={click}>Копочка</button>
             <hr />
-            {/**<div>{text},{n},{count}</div>*/}
-            <div>{text}</div>
+            <div>{text},{n},{count}</div>
         </div>
     )
+
 }
 export default Search;
+
+
+// // Search =>
+// //     document.createElement("div")
+// ...
+// //     document.createElement("input")
+// ...
+// //     button
+// ...
+// //     div2
+// ...
+// //     div.append(input,button,div2)
+
+/**
+ * <div onclick="foo()"></div>
+ * 
+ * <div.onclick=function(){}
+ * div.addEventListner("click", foo)
+ * <div onClick={foo}>
+ */
+/**   Жизненый цикл компанента :
+ *    0 Рендеринг
+ *         Render
+ * 
+ *    1 Монтировка (монтаж приложений)
+ * componentWillMount- я собираюсь собирать(Смонтировать)
+ * componentDidMount- я сделал (смонтировал)
+ * 
+ *   2 Размонтировка (разбор приложения)
+ * componentWillUnmount - ты планируешь разрушеться 
+ * componentDidUnmount - ты разрушелся 
+ * componenyWillUpdate - ты обновляйся пожалуйста 
+ * componenyDidUpdate - после того как я обновился, что то изменилось
+ *  
+ */
